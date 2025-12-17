2731c1386ae8294d5b72f18c2fc134b2542c05fc
kittujadav83@gmail.com <Kittu@1379>
2025-12-17 21:13:05 +0530

    multiply code

---
+++ multi.py
@@ -0,0 +1,5 @@
+num = int(input("Enter the number : = " ))
+print(f"Multiplication table of {num} is :")
+for i in range(1 , 10):
+# print(str(num) + "x" + str(i) + "=" + str(num*i))
+ print(f"{num} X  {i} = {num*i}")
