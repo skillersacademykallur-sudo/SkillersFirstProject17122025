18a31b277cc3253fc95cee458be6c91061acfc0d
vamshi7925-source <vamshi.7925@gmail.com>
2025-12-19 17:31:28 +0530

    processing

--- vamshi.py
+++ vamshi.py
@@ -1,1 +1,20 @@
-print("vamshi")
+#print("vamshi")
+
+#Here Age = A
+
+A =0
+
+if A < 16:
+  print("Don't use mobile phones")
+
+if A in range (16,25):
+  print("improving your skills and knowledge")
+
+if A in range (25,40):
+  print("Focus on career and income skills")
+
+if A in range (40,65):
+  print("Take responsibility and remain active")
+
+if A > 65:
+  print("Focus on health and take proper rest")
